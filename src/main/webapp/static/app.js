3dfdcacd744cb2beb441a3f15a07c1e3eaaff52b
kriss <kriss.pujats@finabay.com>
2017-04-19 16:52:50 +0300

    use 127.0.0.1

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -6,7 +6,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-	var socket = new SockJS('http://localhost:8123/nfc');
+	var socket = new SockJS('http://127.0.0.1:8123/nfc');
 	stompClient = Stomp.over(socket);
 	stompClient.connect({},
 		function (frame) {
