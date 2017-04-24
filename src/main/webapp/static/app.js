3d385ea9148aa2664f7e9794279610d6914288c8
kriss <kriss.pujats@gmail.com>
2017-04-25 00:34:56 +0300

    rename nfc status to type

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -15,7 +15,7 @@ function connect() {
 		console.log('Connected: ' + frame);
 		stompClient.subscribe('/topic/nfc', function (nfcNotice) {
 			var nfcJson = JSON.parse(nfcNotice.body);
-			showText('type=' + nfcJson.status + '; uid=' + nfcJson.uid);
+			showText('type=' + nfcJson.type + '; uid=' + nfcJson.uid);
 		});
 		},
 		function (message) {
